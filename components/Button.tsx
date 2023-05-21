319d56a6f7539735ec624f5e6e23fcda1bb1bc06
Tyler Mueller <muelltyl@gmail.com>
2023-05-21 15:04:55 -0500

    fixes Button error

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,7 +1,7 @@
 import { JSX } from "preact";
 import { IS_BROWSER } from "$fresh/runtime.ts";
 
-export function Button(props: JSX.HTMLAttributes<HTMLButtonElement>): HTMLButtonElement {
+export function Button(props: JSX.HTMLAttributes<HTMLButtonElement>) {
   return (
     <button
       {...props}
