19692895b4560f8869ff774cc968a6a2467fae1a
chnoma <85920821+chnoma@users.noreply.github.com>
2023-05-21 19:48:11 -0500

    Changed index to login

--- routes/index.tsx
+++ routes/index.tsx
@@ -6,22 +6,32 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Fresh App</title>
+        <title>Log In</title>
       </Head>
-      <body class="bg-black text-white">
-        <div class="p-4 mx-auto max-w-screen-md ">
-          <img
-            src="/logo.svg"
-            class="w-32 h-32"
-            alt="the fresh logo: a sliced lemon dripping with juice"
-          />
-          <p class="my-6">
-            Welcome to `grokked`. Try updating this message in the ./routes/index.tsx
-            file, and refresh.
-          </p>
-          <Post author={"me"} title={"test post"} body="Hello" />
+      <body class="flex h-screen bg-gray-600">
+        <div class="w-full max-w-xs m-auto bg-gray-100 rounded p-5">
+          <header>
+            <img class="w-20 mx-auto mb-5" src="https://avatars.githubusercontent.com/u/85920821?v=4"/>
+          </header>
+          <form action="/api/auth">
+            <div>
+            <label class="block mb-w text-orange-500" for="username">Username</label>
+              <input class="w-full p-2 mb-6 text-orange-800 border-b-2 border-orange-950 outline-none focus:bg-gray-300" 
+                      type="text"
+                      name="username" />
+            </div>
+            <div>
+              <label class="block mb-w text-orange-500" for="password">Password</label>
+              <input class="w-full p-2 mb-6 text-orange-800 border-b-2 border-orange-950 outline-none focus:bg-gray-300" 
+                      type="password"
+                      name="password" />
+            </div>
+            <div>
+              <input class="w-full bg-gray-900 hover:bg-pink-600 text-white efont-bold py-2 px-4 mb-6 rounded" type="submit"/>
+            </div>
+          </form>
         </div>
-      </body>
+    </body>
     </>
   );
 }
